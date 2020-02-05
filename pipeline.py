17fcfa1f1099f1ddd63656712c16fe62b35f20e5
sbibek <sbibek@hotmail.com>
2020-02-05 13:43:24 -0800

    zeek run

--- pipeline.py
+++ pipeline.py
@@ -1,6 +1,8 @@
 import sys
 import subprocess
 import time
+from os import listdir
+from os.path import isfile, join
 
 # This specific pipeline is for the cnc-bot communication
 
@@ -16,11 +18,22 @@ temp_dir = 'temp'
 def log(msg):
     print("[{}] {}".format('PL', msg))
 
+
 def clear_temp_dir():
     proc = subprocess.Popen(['rm', 'rf', 'temp'])
     proc.wait()
     proc2 = subprocess.Popen(['mkdir', 'temp'])
     proc2.wait()
+
+
+def get_files_in_temp_dir():
+    return [f for f in listdir(temp_dir) if isfile(join(temp_dir, f))]
+
+def run_zeek(pcapfile, brofile, logfile):
+    command = ['zeek', '-C', '-r', 'pcapfile', 'brofile']
+    proc = subprocess.Popen(command)
+    proc.wait()
+
 
 
 def filter_pcap():
@@ -30,6 +43,8 @@ def filter_pcap():
                                 _pcap_filter, '-w', filtered_pcap_file, '-2'])
     proc.wait()
 
+
+
 def split_filtered_file():
     clear_temp_dir()
     command = ['./PcapSplitter', '-f', filtered_pcap_file, '-o', temp_dir, '-m', 'connection'] 
@@ -37,5 +52,13 @@ def split_filtered_file():
     proc = subprocess.Popen(command)
     proc.wait()
 
+
+def run_zeek_on_splitted_files():
+    files = get_files_in_temp_dir()
+    log("There are {} files corresponding to {} connections in the filtered file".format(len(files), len(files)))
+    for file in files:
+        run_zeek("{}/{}".format(temp_dir, file),'processcnc.bro', '')
+
 filter_pcap();
 split_filtered_file();
+run_zeek_on_splitted_files();
